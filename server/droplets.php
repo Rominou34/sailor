51ed3d402efdfcc3a94486b23ebf6b07bcd1d627
Romain Arnaud <arnaud.romain34@gmail.com>
2017-03-14 19:18:14 +0100

    Actually use the data from the API

--- server/droplets.php
+++ server/droplets.php
@@ -3,33 +3,33 @@
 require('data.php');
 
 // Fake data so I don't have to request DO servers everytime
-echo($droplets);
+//echo($droplets);
 
-// session_start();
-//
-// if(isset($_SESSION["access_token"])) {
-//   try {
-//     $ch = curl_init();
-//
-//     if (FALSE === $ch)
-//         throw new Exception('failed to initialize');
-//
-//     $token_url = "https://api.digitalocean.com/v2/droplets";
-//     curl_setopt($ch, CURLOPT_URL, $token_url);
-//     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-//         'Content-Type: application/json',
-//         'Authorization: Bearer '.$_SESSION["access_token"]
-//         ));
-//
-//     $content = curl_exec($ch);
-//
-//     if (FALSE === $content)
-//         throw new Exception(curl_error($ch), curl_errno($ch));
-//
-//     echo($content);
-//   } catch(Exception $e) {
-//     echo($e->getCode()."<br/>". $e->getMessage());
-//   }
-// }
+session_start();
+
+if(isset($_SESSION["access_token"])) {
+  try {
+    $ch = curl_init();
+
+    if (FALSE === $ch)
+        throw new Exception('failed to initialize');
+
+    $token_url = "https://api.digitalocean.com/v2/droplets";
+    curl_setopt($ch, CURLOPT_URL, $token_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Authorization: Bearer '.$_SESSION["access_token"]
+        ));
+
+    $content = curl_exec($ch);
+
+    if (FALSE === $content)
+        throw new Exception(curl_error($ch), curl_errno($ch));
+
+    echo($content);
+  } catch(Exception $e) {
+    echo($e->getCode()."<br/>". $e->getMessage());
+  }
+}
 ?>
